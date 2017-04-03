9d75ff2a023d4bd90074f148041bd72da1cd775f
Carl Corder <ccorder@xpanxion.com>
2017-04-03 11:30:32 -0500

    version 1.4.4 update

--- gears4-pack-stats/background/cards-recorder.js
+++ gears4-pack-stats/background/cards-recorder.js
@@ -3,18 +3,61 @@
  * and then listens for messages from content. Packs are recorded to the
  * sheet with the specified sheet id.
  **/
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
+chrome.browserAction.onClicked.addListener(() => openInNewTab(" https://gearsofwar.com/cards"));
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log("Attempting to recording pack...");
+        console.log("Received pack:");
+        console.table(request);
         background.recordPack(request);
-    });
+    }
+);
 
-function onResponse(error, status, response) {
-    var message = (!error && status == 200)
-        ? JSON.parse(response).updates.updatedRange
-            : error;
-    background.sendMessage({status: status, message: message});
-}
+// function executeContent() {
+//     chrome.tabs.query({
+//         url: "https://gearsofwar.com/*/cards/my-packs/*"
+//     }, function(tabs) {
+//         if (tabs.length > 0) {
+//             chrome.tabs.sendMessage(tabs[0].id, {
+//                 status: 100,
+//                 message: "continue"
+//             }, function(response) {
+//                 if (response) {
+//                     // do nothing, content script is already loaded
+//                     console.debug("Content script already loaded.");
+//                 } else {
+//                     console.debug("Loading content script.");
+//                     chrome.tabs.executeScript(tabs[0].id, {
+//                         file: "content/cards-collector.js"
+//                     });
+//                 }
+//             });
+//         } else {
+//             console.warn("https://gearsofwar.com/*/cards/my-packs/* tab not found.");
+//         }
+//     });
+// }
+
+// execute the content script whenever user navigates to packs page
+// http://www.toptip.ca/2010/01/google-chrome-content-script.html
+// http://stackoverflow.com/questions/20865581/chrome-extension-content-script-not-loaded-until-page-is-refreshed
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        if (/https:\/\/gearsofwar.com\/.*\/cards\/my-packs\/.*/.test(tabs[0].url)) {
+            chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+            // executeContent();
+        }
+    });
+});
 
 /**
  * There are some cases, such as when the user changes their password, when non-expired access tokens will stop working.
@@ -24,83 +67,120 @@ function onResponse(error, status, response) {
  * You can see the current state of the token cache on chrome://identity-internals.
  * See: https://developer.chrome.com/apps/app_identity#getAuthToken-caching for more information.
  **/
-// callback = function (error, httpStatus, responseText);
-function xhrWithAuth(method, url, rowData, callback) {
-    var access_token;
-    var retry = true;
-    getToken();
+var post = {
 
-    function getToken() {
+     oAuthRetry: true,
+     errorRetry: true,
+
+    // callback = function (error, httpStatus, responseText);
+    xhrWithAuth(url, rowData, callback) {
+        var access_token;
+        getToken();
+
+        function getToken() {
+            chrome.identity.getAuthToken({
+                interactive: false
+            }, function(token) {
+                if (chrome.runtime.lastError) {
+                    callback(chrome.runtime.lastError);
+                    return;
+                }
+                access_token = token;
+                requestStart();
+            });
+        }
+
+        function requestStart() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url);
+            xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+            xhr.onload = requestComplete;
+            xhr.send(JSON.stringify(rowData));
+        }
+
+        function requestComplete() {
+            if (this.status == 401) {
+                chrome.identity.removeCachedAuthToken({
+                        token: access_token
+                    },
+                    getToken);
+            } else {
+                callback(null, this.status, this.response);
+            }
+        }
+    },
+
+    getTokenInteractive() {
         chrome.identity.getAuthToken({
             interactive: true
         }, function(token) {
-            if (chrome.runtime.lastError) {
-                callback(chrome.runtime.lastError);
+            if (post.errorRetry) {
+                post.errorRetry = false;
+                access_token = token;
                 return;
             }
-            access_token = token;
-            requestStart();
+        });
+    },
+
+    onResponse(error, status, response) {
+        let message;
+        if(!error && status == 200) {
+            message = JSON.parse(response).updates.updatedRange;
+        } else if(!error) {// 401 Unauthenticated or 403 Unauthorized
+            message = JSON.parse(response).error.message;
+        } else {// runtime error
+            message = error.message;
+            // try authenticating in interactive mode if we error
+            post.getTokenInteractive();
+        }
+        background.sendMessage({
+            status, message
         });
     }
-
-    function requestStart() {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-        xhr.onload = requestComplete;
-        xhr.send(JSON.stringify(rowData));
-    }
-
-    function requestComplete() {
-        if (this.status == 401 && retry) {
-            retry = false;
-            chrome.identity.removeCachedAuthToken({
-                    token: access_token
-                },
-                getToken);
-        } else {
-            callback(null, this.status, this.response);
-        }
-    }
-}
+};
 
 /**
  * See: https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
  * for more details on the sheets rest api v4.
  **/
-
 var sheet = {
 
     baseUrl: "https://sheets.googleapis.com/v4/spreadsheets/",
     id: "1JMSBn2s6GQxhn9ylj2INB0kQFF0G7tqMLT4y0p31upk",
     queryParams: "valueInputOption=USER_ENTERED",
 
-    url: function(range) {
+    url(range) {
         return sheet.baseUrl + sheet.id + "/values/" + range + ":append?" + sheet.queryParams;
     },
 
-    value: function(row) {
-        return {"values": [row]};
+    value(row) {
+        return {
+            "values": [row]
+        };
     }
 
-}
+};
 
 var background = {
 
-    recordPack: function(pack) {
-        xhrWithAuth("POST", sheet.url(pack.type + "!A:H"),
+    recordPack(pack) {
+        post.xhrWithAuth(sheet.url(pack.type + "!A:H"),
             sheet.value([null, pack.date, pack.gamertag, ...pack.cards]),
-        onResponse);
+                post.onResponse);
     },
 
-    sendMessage: function(message) {
+    sendMessage(message) {
         chrome.tabs.query({
-            url: "https://gearsofwar.com/*/cards/my-packs*"
+            url: "https://gearsofwar.com/*/cards/my-packs/*"
         }, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-                console.log("Sent message : " + message);
+                console.log("Recording summary:");
+                console.table([message]);
             });
         });
     }
 
 };
+
+// executeContent();
+console.debug("Background loaded.");
--- gears4-pack-stats/content/cards-collector.js
+++ gears4-pack-stats/content/cards-collector.js
@@ -3,8 +3,9 @@
  * https://gearsofwar.com/{locale}/cards/my-packs/{pack-type}
  * and sends data to the background script cards-recorder.js
  **/
+ 
 var content = {
-    sendPackData: function() {
+    sendPackData() {
         // sends a message to background
         chrome.runtime.sendMessage({
             type: pack.getType(),
@@ -12,70 +13,163 @@ var content = {
             gamertag: pack.getGamerTag(),
             date: pack.getDate()
         }, function(response) {
-            console.log(response);
+            console.info("Pack sent to background process.");
         });
     }
 };
 
-/**
- * var cardCategories = Array.from(document.querySelectorAll('.cardAssembleCategory')).map(card => card.textContent);// ["Skin", "Engineer Skill", "Scout Skill"]
- * var cardRarity = Array.from(document.querySelectorAll('.cardListImage')).map(card => card.alt).map(rarity => rarity.split(" ")[0]);// ["common", "common", "rare"]
- **/
 var pack = {
 
-    getCardUids: function() {
+    getCardUids() {
         var cardUids = Array.from(document.querySelectorAll('img.assembleContent')).map(img => img.currentSrc).map(url => url.split("/")).map(path => path[6])
         return cardUids;
     },
 
-    getCardTitles: function() {
+    getCardTitles() {
         var cardTitles = Array.from(document.querySelectorAll('p.cardAssembleTitle')).map(card => card.textContent);
         return cardTitles;
 
     },
 
-    getType: function() {
+    getCardRarities() {
+        var cardRarities = Array.from(document.querySelectorAll('.cardListImage')).map(card => card.alt).map(rarity => rarity.split(" ")[0]);
+        return cardRarities;
+    },
+
+    // used by injectMessage to generate inner HTML
+    getCards() {
+        var cards = [];
+        var cardTitles = pack.getCardTitles();
+        var cardRarities = pack.getCardRarities();
+        cardTitles.forEach((value, index) => cards.push({
+            title: value,
+            rarity: cardRarities[index]
+        }));
+        return cards;
+    },
+
+    getType() {
         var packType = document.querySelector('.packOpenHeader .text-nowrap').innerText.trim();
         return packType
     },
 
-    getGamerTag: function() {
+    getGamerTag() {
         var gamertag = JSON.parse(document.getElementById('initialState').textContent).user.gamertag;
         return gamertag;
     },
 
-    getDate: function() {
+    getDate() {
         var date = new Date().toISOString().slice(0, 10); // YYYY-MM-DD
         return date;
     }
 
 };
 
-function readyToReveal(event){
+function readyToReveal(event) {
     var element = event.target;
     if(element.id === "reveal-all-desktop"){
+        element.disabled = true;
         content.sendPackData();
     }
 }
 
-// send pack data when 'Reveal All' button is clicked
-document.addEventListener("click", readyToReveal);
+// add event listeners if we are on the correct page
+// https://triangle717.wordpress.com/2015/12/14/js-avoid-duplicate-listeners/
+if (/https:\/\/gearsofwar.com\/.*\/cards\/my-packs\/.*/.test(window.location.href)) {
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        // user feedback
-        if(request.status === 200) {
-            injectMessage(pack.getCardTitles().join(', ') + " saved to " + request.message);
-        } else {
-            injectMessage(request.message);
+    // send pack data when 'Reveal All' button is clicked
+    document.body.addEventListener("click", readyToReveal);
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.status === 100) {
+                sendResponse("OK");
+            } else {
+                injectMessage(request);
+            }
         }
-    }
-);
+    );
 
-function injectMessage(msg) {
-    var toolbar = document.querySelector('[role="toolbar"]');
-    var h4 = document.createElement('h4');
-    h4.style.color = "#FFFFFF"; h4.align = "center";
-    h4.innerText = msg;
+}
+
+// rarity => color mapping
+var colors = new Map();
+colors.set("common",    "#089F08");
+colors.set("rare",      "#48A0A9");
+colors.set("epic",      "#9745A5");
+colors.set("legendary", "#C2B75A");
+
+// injects a message above the toolbar div
+function injectMessage(request) {
+    let dashboardUrl = "https://docs.google.com/spreadsheets/d/1JMSBn2s6GQxhn9ylj2INB0kQFF0G7tqMLT4y0p31upk/view#gid=1614938261";
+    let {status, message} = request;// es6 destructuring
+    let toolbar = document.querySelector('[role="toolbar"]');
+    let h4 = document.createElement('h4');
+    h4.align = "center";
+    if(status === 200) {
+        let cards = pack.getCards();
+        h4.style.color = "#FFFFFF";
+        h4.innerHTML = `${cards.map(card => `<span style="color:${colors.get(card.rarity)}">${card.title}</span>`).join(', ')} 
+                            saved to <a href=${dashboardUrl} target="_blank">${message}</a>`;// link to dashboard
+        console.info("Pack saved to sheet.");
+    } else {
+        h4.style.color = "#FF0033";
+        h4.innerHTML = message;
+    }
     toolbar.parentElement.insertBefore(h4, toolbar);
 }
+
+function displayVersion() {
+    if (document.getElementById('pack-stats-version') == null) {
+        let manifest = chrome.runtime.getManifest();
+        let backToTop = document.getElementById('back-to-top');
+        let h4 = document.createElement('h4');
+        h4.id = "pack-stats-version";
+        h4.align = "center";
+        h4.style.color = "#777780";
+        h4.innerHTML = `Using ${manifest.name} version ${manifest.version}`;
+        backToTop.parentElement.insertBefore(h4, backToTop);
+    }
+}
+
+function waitForCards() {
+    let cardsLoaded = false;
+    let cards = document.querySelector('.revealPack .card-flex');
+    if(cards) {
+        let revealAllBtn = document.getElementById('reveal-all-desktop');
+        revealAllBtn.disabled = true;
+        if (Array.from(cards.children, child => child.childElementCount).reduce((a, b) => a + b, 0) === cards.childElementCount) {
+            revealAllBtn.disabled = false;
+            cardsLoaded = true;
+        }
+    }
+    if(!cardsLoaded) {
+        setTimeout(waitForCards, 0);
+    }
+}
+
+var mainObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        let item = mutation.addedNodes.item(0);
+        if (item != null && item.id === "main") {
+            let packOpen = document.getElementById('pack-open');
+            if (packOpen) {
+                packOpen.addEventListener("click", waitForCards);
+            }
+        }
+    });
+});
+ 
+// mutation notification level & configuration
+var observerConfig = {
+    //attributes: true, 
+    childList: true, 
+    //characterData: true 
+};
+ 
+// listen to changes on app-panel and child nodes
+var appPanel = document.getElementById('app-panel');
+mainObserver.observe(appPanel, observerConfig);
+
+displayVersion();
+console.debug("Gears4 Pack Stats: content script loaded.");
